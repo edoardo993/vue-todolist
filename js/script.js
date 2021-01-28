6bdd642e59f8a07d56755cccb4679830362b4252
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-01-28 16:32:28 +0100

    fine prima parte

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-
+        newWord: '',
+        wordsList: [],
+        remove: false
     },
     methods: {
-        
+        control: function(){
+            if(this.newWord==='' || !isNaN(this.newWord)){
+                alert('Devi inserire qualcosa e che non sia un numero!')
+            }else if(this.newWord.length>100){
+                alert('Non puoi inserire testi così lunghi!')
+            }else{
+                this.wordsList.push(this.newWord);
+                console.log(this.wordsList)
+            }
+        }
     }
-})
+});
+Vue.config.devtools = true;
