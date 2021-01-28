bf81f75a04481ef5a8cbc9f6f51452f623dd8abc
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-01-28 19:02:51 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,24 +3,32 @@ new Vue({
     data: {
         newWord: '',
         wordsList: [],
-        remove: false
+        remove: false,
+        warningClass: 'hide',
+        warningText: '',
+        illegalChars: '\|"£$%&/()=?^+*§°ç[]{}#@`'
     },
     methods: {
         isValid: function(word){
+            this.warningClass='warning';
             if(word===''){
-                alert('Devi inserire qualcosa!');
+                this.warningText='Devi inserire un item nel campo'
+                return false
+            }
+            if(this.illegalChars.includes(word)){
+                this.warningText='Non puoi inserire caratteri speciali'
                 return false
             }
             if(!isNaN(word)){
-                alert('Non puoi inserire un numero!');
+                this.warningText='Non puoi inserire un numero';
                 return false
             }
             if(word.length>100){
-                alert('Non puoi inserire testi così lunghi!');
+                this.warningText='Non puoi inserire items così lunghi';
                 return false
             }
             if(this.wordsList.includes(word)){
-                alert('Non puoi inserire la stessa cosa due volte!');
+                this.warningText='Non puoi inserire lo stesso item due volte';
                 return false
             }
             return true
@@ -28,11 +36,15 @@ new Vue({
         control: function(){
             if(this.isValid(this.newWord)){
                 this.wordsList.push(this.newWord);
-                console.log(this.wordsList)
+                this.warningClass='hide';
+                this.warningText=''
             }
         },
         // prova funzione al click
         exClick: function(){
+            this.wordsList.filter((element)=>{
+                return this.wordsList.includes(element)
+            })
             if(this.wordsList.length!==-1){
                 alert('ciaone')
             }
