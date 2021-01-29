cd62ce7b0ada917ff137bd810531a17a2fa932f1
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-01-29 08:43:42 +0100

    exercise completed

--- js/script.js
+++ js/script.js
@@ -6,24 +6,22 @@ new Vue({
         remove: false,
         warningClass: 'hide',
         warningText: '',
-        illegalChars: '\|"£$%&/()=?^+*§°ç[]{}#@`!'
+        illegalChars: '/^[a-zA-Z0-9\_\*\-\+\!\?\,\:\;\.\xE0\xE8\xE9\xF9\xF2\xEC\x27]/'
     },
     methods: {
         isValid: function(word){
             this.warningClass='warning';
+            for(var x=0; x<word.length; x++){
+                if(this.illegalChars.includes(word[x])){
+                    this.warningText='Non puoi inserire caratteri speciali';
+                    return false
+                }
+            }
             if(word===''){
                 this.warningText='Devi inserire un item nel campo';
                 return false
             }
-            if(this.illegalChars.includes(word)){
-                this.warningText='Non puoi inserire caratteri speciali';
-                return false
-            }
-            if(!isNaN(word)){
-                this.warningText='Non puoi inserire un numero';
-                return false
-            }
-            if(word.length>100){
+            if(word.length>50){
                 this.warningText='Non puoi inserire items così lunghi';
                 return false
             }
@@ -35,15 +33,14 @@ new Vue({
         },
         control: function(){
             if(this.isValid(this.newWord)){
-                this.wordsList.push(this.newWord);
+                this.wordsList.push(this.newWord.charAt(0).toUpperCase() + this.newWord.substring(1).toLowerCase());
                 this.warningClass='hide';
                 this.warningText=''
             }
         },
-        // prova funzione al click
-        exClick: function(indexItem){
+        exClick: function(itemIndex){
             this.wordsList=this.wordsList.filter((element, index)=>{
-                if(indexItem!==index){
+                if(itemIndex!==index){
                     return element
                 }
             })
